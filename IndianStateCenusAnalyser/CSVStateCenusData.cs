efc85e12c170c58635a224d9ce50a0445799dc30
nibedita260 <deepi.nibedita260.com>
2021-10-20 23:48:20 +0530

    [Nibedita] Add: LoadStatesCodeCsvDataAndTest For IndianStateCenusAnalyser Features Implemented

--- IndianStateCenusAnalyser/CSVStateCenusData.cs
+++ IndianStateCenusAnalyser/CSVStateCenusData.cs
@@ -12,6 +12,8 @@ namespace IndianStateCenusAnalyser
     {
         public string csvFilePath = @"D:\git\IndianStateCenusAnalyser\IndianStateCenusAnalyser\Files\StateCensusData.csv";
         public string fileHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
+        public string statesCodePath = @"D:\git\IndianStateCenusAnalyser\IndianStateCenusAnalyser\Files\StateCode.csv";
+        public string statesCodeFileHeader = "SrNo,State,Name,TIN,StateCode";
         public object LoadData(string csvFilePath,string fileHeaders)
         {
             int numberOfRecord = 0;
@@ -61,6 +63,56 @@ namespace IndianStateCenusAnalyser
                 Console.WriteLine(e.Message);
             }
             return numberOfRecord;
+        }
+        public object LoadStatesCodeData(string statesCodePath, string statesCodeFileHeader)
+        {
+            int numberOfRecords = 0;
+            try
+            {
+                if (Path.GetExtension(statesCodePath) != ".csv")
+                {
+                    throw new CenusAnalyserCustomException(CenusAnalyserCustomException.InvalidCenusdetails.INCORRECT_FILE_TYPE, "Incorrect File Type");
+                }
+                if (!File.Exists(statesCodePath))
+                {
+                    throw new CenusAnalyserCustomException(CenusAnalyserCustomException.InvalidCenusdetails.FILE_NOT_FOUND, "File Not Found");
+                }
+                StreamReader sr = new StreamReader(statesCodePath);
+                string line;
+                string[] rowData = new string[100];
+                rowData = File.ReadAllLines(statesCodePath);
+                if (rowData[0] != statesCodeFileHeader)
+                {
+                    throw new CenusAnalyserCustomException(CenusAnalyserCustomException.InvalidCenusdetails.INVALID_HEADERS, "Invalid Headers");
+                }
+                while ((line = sr.ReadLine()) != null)
+                {
+                    numberOfRecords++;
+                    //iterate the states code csv data
+                    foreach (string csvStatesCodeData in rowData)
+                    {
+                        if (!csvStatesCodeData.Contains(","))
+                        {
+                            throw new CenusAnalyserCustomException(CenusAnalyserCustomException.InvalidCenusdetails.INVALID_DELIMITER, "Invalid Delimiters In File");
+                        }
+                        else
+                        {
+                            Console.WriteLine("{0}" + "\t", csvStatesCodeData.Replace(",", " "));
+                            Console.WriteLine("\t");
+                        }
+                    }
+                }
+                Console.WriteLine("total number of records:" + numberOfRecords);
+            }
+            catch (CenusAnalyserCustomException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return numberOfRecords;
         }
     }
 }
--- IndianStateCenusAnalyser/Program.cs
+++ IndianStateCenusAnalyser/Program.cs
@@ -6,10 +6,31 @@ namespace IndianStateCenusAnalyser
     {
         static void Main(string[] args)
         {
-            string Path = @"D:\git\IndianStateCenusAnalyser\IndianStateCenusAnalyser\Files\StateCensusData.csv";
+            string path = @"D:\git\IndianStateCenusAnalyser\IndianStateCenusAnalyser\Files\StateCensusData.csv";
             string fileHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
-            CSVStateCenusData cSVStateCenusData = new CSVStateCenusData();
-            cSVStateCenusData.LoadData(Path, fileHeaders);
+            string statesCodePath = @"D:\git\IndianStateCenusAnalyser\IndianStateCenusAnalyser\Files\StateCode.csv";
+            string statesCodeFileHeader = "SrNo,State,Name,TIN,StateCode";
+            int options;
+            bool isExit = false;
+            while (!isExit)
+            {
+                Console.WriteLine("choose 1.AnalyseIndianStateCSVData 2.AnalyseIndianStatesCodeCSVData");
+                options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        CSVStateCenusData cSVStateCenusData = new CSVStateCenusData();
+                        cSVStateCenusData.LoadData(path, fileHeaders);
+                        break;
+                    case 2:
+                        CSVStateCenusData cSVStatesCodeCenusData = new CSVStateCenusData();
+                        cSVStatesCodeCenusData.LoadStatesCodeData(statesCodePath, statesCodeFileHeader);
+                        break;
+                    default:
+                        Console.WriteLine("Choose valid options");
+                        break;
+                }
+            }
         }
     }
 }
